ea2ad028da2d8514e4055081ba5da51c3eac907b
Dinesh12 <55702844+dineshkannan010@users.noreply.github.com>
2020-12-20 21:34:19 +0530

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,38 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Languages</a>
+<ul>
+<li><a href="create-brand.php">Create Language</a></li>
+<li><a href="manage-brands.php">Manage Language</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-movies"></i> Movies</a>
+					<ul>
+						<li><a href="post-avehicle.php">Post a Movie</a></li>
+						<li><a href="manage-vehicles.php">Manage Movies</a></li>
+					</ul>
+				</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Bookings</a>
+					<ul>
+						<li><a href="new-bookings.php">New</a></li>
+						<li><a href="confirmed-bookings.php">Confirmed</a></li>
+						<li><a href="canceled-bookings.php">Canceled</a></li>
+					</ul>
+				</li>
+
+		
+
+
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+
+
+			</ul>
+		</nav>
